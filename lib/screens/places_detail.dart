36b61ef2592ab67fac309aee5bd3141837729699
Yoga S Devanada <ydevanada@gmail.com>
2023-06-06 20:21:35 +0700

    feat: Initiate project

---
+++ lib/screens/places_detail.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'package:favorite_places/models/place.dart';
+
+class PlacesDetailScreen extends StatelessWidget {
+  const PlacesDetailScreen({super.key, required this.place});
+
+  final Place place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(place.title),
+      ),
+      body: Stack(
+        children: [
+          Image.file(
+            place.image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ],
+      ),
+    );
+  }
+}
